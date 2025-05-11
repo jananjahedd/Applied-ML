21cbf5223d3c3639121c65704145a005b95ce73f
Frano Rajkovic <cf.rajkovic@student.rug.nl>
2025-05-11 13:05:20 +0200

    Add patient_info_from_filepath function

--- src/patient.py
+++ src/patient.py
@@ -1,6 +1,13 @@
 """Patient class."""
 
 from enum import Enum
+from os.path import basename
+
+import mne
+
+from src.utils.logger import get_logger
+
+logger = get_logger(__name__)
 
 
 class Sex(Enum):
@@ -51,3 +58,39 @@ class Patient:
             str: String representation of the Patient class
         """
         return f"Patient #{self.number}: Age-{self.age}, Sex-{self.sex.value}"
+
+
+def patient_from_filepath(file_path: str) -> Patient:
+    """Extract the patient information from the file_path.
+
+    Args:
+        file_path (str): The file path.
+
+    Returns:
+        Patient: The patient information.
+    """
+    raw = mne.io.read_raw_edf(file_path, preload=True)
+    patient_info = raw.info["subject_info"]
+
+    # Get the patient name from the filename
+    file_name = basename(file_path)
+
+    if file_name.startswith("ST7"):  # Telemetry
+        # Files are named in the form ST7ssNJ0-PSG.edf where ss is the
+        # subject number, and N is the night.
+        number = file_name.split("ST7")[1][:2]
+
+    elif file_name.startswith("SC4"):  # Cassette
+        # Files are named in the form SC4ssNEO-PSG.edf where ss is the
+        # subject number, and N is the night.
+        number = file_name.split("SC4")[1][:2]
+
+    else:
+        logger.error("Unknown file name format")
+        raise ValueError("Unknown file name format")
+
+    return Patient(
+        number=int(number),
+        age=patient_info["last_name"].split("yr")[0],
+        sex=patient_info["first_name"],
+    )
