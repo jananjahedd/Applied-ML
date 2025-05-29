43e23a70ccea236013994b12d9fcbb8fde734efd
Frano Rajkovic <f.rajkovic@student.rug.nl>
2025-05-29 11:45:56 +0200

    Add new endpoints to main

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 """This is a sample Python script."""
 
+from fastapi import FastAPI  # type: ignore
 
-def hello_world() -> str:
-    """Returns Hello, World!"""
-    return "Hello, World!"
+from src.endpoints.files import router as files_router
+from src.endpoints.patient import router as patient_router
+from src.endpoints.pipeline import router as pipeline_router
 
+app = FastAPI(title="Placeholder", version="1.0.0", description="Placeholder")
 
-if __name__ == "__main__":
-    hello_world()
+app.include_router(files_router)
+app.include_router(patient_router)
+app.include_router(pipeline_router)
