d1344e6e414e3bb953d696c9121b42f418e64f6f
Frano Rajkovic <f.rajkovic@student.rug.nl>
2025-06-12 18:16:55 +0200

    Removed data endpoint - no longer used

--- backend/src/endpoints/data.py
+++
@@ -1,8 +0,0 @@
-"""Shared data store for the application."""
-
-from typing import Dict
-
-from src.utils.patient import Patient
-
-patients: Dict[int, Patient] = {}
-selected_filenames: Dict[int, str] = {}
